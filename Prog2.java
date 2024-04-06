d0116827371c809e887209f1a11aea05759220c3
Ayan Kumar Dey <ayankumardey99@gmail.com>
2024-04-07 00:48:38 +0530

    Program2 @AyanKD Java

---
+++ Prog2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Prog2 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number:-");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number:-");
+        int b = sc.nextInt();
+        int add = (a+b);
+        System.out.println("The addition of two numbers is = "+add);
+    }
+}
