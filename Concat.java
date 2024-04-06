44abcdd068abcb7b4e0522aaa7ede17bbaf96756
Ayan Kumar Dey <ayankumardey99@gmail.com>
2024-04-07 01:12:30 +0530

    Program3 concatenation of strings @AyanKD Java

---
+++ Concat.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Concat {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:-");
+        java.lang.String name;
+        name = sc.nextLine();
+        System.out.println("Enter your place:-");
+        java.lang.String place;
+        place = sc.nextLine();
+        java.lang.String concat;
+        concat = "Name:-"+name + " Place:-" + place;
+        System.out.println("The concatenated string is = "+concat);
+    }
+}
--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
-public class Prog1 {
+public class Hello {
     public static void main(String args[]){
         System.out.println("Hello World");
     }
