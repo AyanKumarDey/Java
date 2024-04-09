cc4da3441a6f3d47e38aec35ee179c48c45c4599
Ayan Kumar Dey <ayankumardey99@gmail.com>
2024-04-09 23:31:03 +0530

    Quadratic @AyanKD Java

---
+++ Quadratic.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Quadratic {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Enter the three values of a quadratic equation:-");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        double r1,r2;
+        r1 = (-b+Math.sqrt((b*b)-(4*a*c)))/(2*a);
+        r2 = (-b-Math.sqrt((b*b)-(4*a*c)))/(2*a);
+        System.out.println("The roots of the quadratic equation is = "+r1+" "+r2);
+    }
+}
