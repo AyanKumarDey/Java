4810f13848dc3a25413dde022ec49bd12c4a9c09
Ayan Kumar Dey <ayankumardey99@gmail.com>
2024-04-09 18:50:01 +0530

    AreaOfTriangle @AyanKD Java

---
+++ AreaOfTriangle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class AreaOfTriangle {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float base,height;
+        System.out.println("Enter the height of the triangle:-");
+        height = sc.nextFloat();
+        System.out.println("Enter the base of the triangle:-");
+        base = sc.nextFloat();
+        float area;
+        area = (0.5f)*base*height;
+        System.out.println("The area of the triangle is = "+area);
+    }
+}
---
+++ out/production/Java-One-Short/README.md
@@ -0,0 +1,2 @@
+# Java-One-Short
+Hello Everyone over here I will provide a complete java one-short tutorial and a basic overview of java in less time.
