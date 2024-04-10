cbecd27844a36d761160af111ab5413ed83195a6
Ayan Kumar Dey <ayankumardey99@gmail.com>
2024-04-10 15:06:01 +0530

    Cuboid @AyanKD Java

---
+++ Cuboid.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Cuboid {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float length,breadth,height;
+        System.out.println("Enter the length of the triangle:-");
+        length = sc.nextFloat();
+        System.out.println("Enter the breadth of the triangle:-");
+        breadth = sc.nextFloat();
+        System.out.println("Enter the height of the triangle:-");
+        height = sc.nextFloat();
+        double volume;
+        volume = length*breadth*height;
+        double TotalArea;
+        TotalArea = 2*((length*breadth)+(height*length)+(height*breadth));
+        System.out.println("The volume of the cuboid is = "+volume);
+        System.out.println("The total area of the cuboid is = "+TotalArea);
+    }
+}
