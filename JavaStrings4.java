168a7e38ea1c7f55ab8830637611d646b3db4e9d
Ayan Kumar Dey <112239058+AyanKumarDey@users.noreply.github.com>
2024-04-21 22:19:28 +0530

    Updated JavaStrings4.java

--- JavaStrings4.java
+++ JavaStrings4.java
@@ -9,23 +9,23 @@ public class JavaStrings4{
         boolean str = s.matches("Hello Java"); //Matching the whole string.
         System.out.println(str);
         String s2 = "a";
-        boolean str1 = s2.matches("[abc]"); //Check's whether the character present
-        // in the string is either a or b or c any one.(Only for strings with single characters).
+        boolean str1 = s2.matches("[abc]"); //Check's whether the alphabet present
+        // in the string is either a or b or c any one.(Only for strings with single alphabet).
         System.out.println(str1);
         String s3 = "d";
-        boolean str2 = s3.matches("[^abc]"); //Check whether the given character a,b or c is present in the string with single character.
+        boolean str2 = s3.matches("[^abc]"); //Check whether the given alphabet a,b or c is present in the string with single alphabet.
         System.out.println(str2);
         String s4 = "a3";
-        boolean str3 = s4.matches("[abc][189]"); //Check both the characters [] = one character.
+        boolean str3 = s4.matches("[abc][189]"); //Check both the alphabets [] = single alphabet with no repeatation.
         // in the string first checks either anyone a,b,c are present or not then it checks the numbers either 1,8 or 9 is present or not.
         System.out.println(str3);
         String s5 = "a1";
         boolean str4 = s5.matches("[a-z][1-9]");
         System.out.println(str4);
         String s6 = "a";
-        boolean str5 = s6.matches("A|B"); //Check whether the single character string contains either A or B not a or b.
+        boolean str5 = s6.matches("A|B"); //Check whether the single alphabet string contains either A or B not a or b.
         System.out.println(str5);
-        boolean str6 = s5.matches("."); //Checks any single character string not two or more characters string.
+        boolean str6 = s5.matches("."); //Checks any single alphabet string not two or more characters string.
         System.out.println(str6);
     }
 }
