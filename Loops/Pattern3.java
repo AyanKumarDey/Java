e3109cd5258cbcc010e9be9c2987fe2fed69c942
Ayan Kumar Dey <ayankumardey99@gmail.com>
2024-05-04 20:50:13 +0530

    Pattern3 @AyanKD Java

---
+++ Loops/Pattern3.java
@@ -0,0 +1,21 @@
+package Loops;
+import java.util.*;
+public class Pattern3 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the max row:-");
+        int r = sc.nextInt();
+        System.out.println("Enter the max column:-");
+        int c = sc.nextInt();
+        System.out.println("The following pattern is:-");
+        for(int i=1;i<=r;i++)
+        {
+            for(int j=1;j<=c;j++)
+            {
+                System.out.print((i+j)+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
