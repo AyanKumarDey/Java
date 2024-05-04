17cab24d5346ccff658a63a6e6ade735bc2d36fa
Ayan Kumar Dey <ayankumardey99@gmail.com>
2024-05-04 18:43:44 +0530

    Pattern2 @AyanKD Java

---
+++ Loops/Pattern2.java
@@ -0,0 +1,21 @@
+package Loops;
+import java.util.*;
+public class Pattern2 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the max rows:-");
+        int r = sc.nextInt();
+        System.out.println("Enter the max columns:-");
+        int c = sc.nextInt();
+        System.out.println("The pattern is:-");
+        for(int i=1;i<=r;i++) //For Rows
+        {
+            for(int j=1;j<=c;j++) // For Columns
+            {
+                System.out.print(i+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
