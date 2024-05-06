ed31b1b95fed955464fb5c9e31be04fc91e28c67
Ayan Kumar Dey <ayankumardey99@gmail.com>
2024-05-07 02:17:46 +0530

    Pattern9 @AyanKD Java

---
+++ Nested_Loops/Pattern9.java
@@ -0,0 +1,28 @@
+package Nested_Loops;
+import java.util.*;
+public class Pattern9 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the max rows:-");
+        int r = sc.nextInt();
+        System.out.println("Enter the max column:-");
+        int c = sc.nextInt();
+        System.out.println("The following pattern is:-");
+        for(int i=1;i<=r;i++)
+        {
+            for(int j=1;j<=c;j++)
+            {
+                if(j<=(c-i))
+                {
+                    System.out.print("  ");
+                }
+                else
+                {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+}
