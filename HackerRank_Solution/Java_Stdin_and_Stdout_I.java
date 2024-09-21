bea956624aaf97a977c7f21ff83f379d6f50e630
AyanKumarDey <ayankumardey99@gmail.com>
2024-09-22 00:00:44 +0530

    Java Standard Input Output @AyanKD Java

---
+++ HackerRank_Solution/Java_Stdin_and_Stdout_I.java
@@ -0,0 +1,15 @@
+package HackerRank_Solution;
+import java.util.*;
+
+public class Java_Stdin_and_Stdout_I {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
